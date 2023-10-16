feb16241a7ac9a94a9a45bb742c0064e3dea7f69
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-16 15:58:33 -0300

    feat: create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 export function App() {
   return (
     <>
       <GlobalStyles />
-      <h1>Hello world</h1>
+      <Header />
     </>
   );
 }
